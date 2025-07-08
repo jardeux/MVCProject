8e54b9883ea610f3ff150b9109ba05d8d3c4994f
jardeux <umut53701@gmail.com>
2025-07-08 23:09:09 +0300

    Initial

---
+++ e-commerenceMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e_commerenceMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
