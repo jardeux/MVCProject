135fef7a807280b027f7e09ee81bfeca072f1216
jardeux <umut53701@gmail.com>
2025-07-11 20:43:06 +0300

    category cotr operations completed

--- e-commerenceMVC/Controllers/CategoryController.cs
+++ e-commerenceMVC/Controllers/CategoryController.cs
@@ -27,20 +27,93 @@ namespace e_commerenceMVC.Controllers
         [HttpPost]
         public IActionResult Create(Category obj) // Bu metod, Create sayfasında form gönderildiğinde tetiklenir.
         {
-            if(obj.Name == obj.DisplayOrder.ToString())
+            //3 adet validation var bunlardan 1i client side javascript ile eklediğimiz sayfa yenilenmeden doğrulama yapar
+            //diğeri asp validation bu controllerdan model error ekleriz viewda <div asp-validation - summary = "All" ></ div > ekleriz
+            //custom validation modelden ekleriz 
+
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("name", "İsim ile display order aynı olamaz."); // Eğer Name ve DisplayOrder aynı ise, ModelState'e özel bir hata eklenir.
             }
-            
-            if(ModelState.IsValid) 
+            if (obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("name", "Test ismi kullanılamaz."); // Eğer Name "test" ise, ModelState'e özel bir hata eklenir.
+            }
+            if (ModelState.IsValid)
             {
                 //ModelState.IsValid kontrolü yapıldıktan sonra veritabanına ekleme işlemi yapılır.
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index"); //Eğer veritabanına ekleme işlemi başarılı ise Index sayfasına yönlendirme yapılır.
             }
-                return View(); //Eğer ModelState.IsValid kontrolü başarısız ise, tekrar Create sayfasına yönlendirme yapılır.
+            return View(); //Eğer ModelState.IsValid kontrolü başarısız ise, tekrar Create sayfasına yönlendirme yapılır.
 
         }
+        public IActionResult Edit(int? id) // Bu metod, Edit sayfasına yönlendirme yapmak için kullanılır. id parametresi nullable int olarak tanımlanmıştır.
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound(); // Eğer id null veya 0 ise, NotFound döndürülür.
+            }
+            Category categoryFromDb = _db.Categories.Find(id); // Veritabanından id'ye göre kategori bulunur.
+            if (categoryFromDb == null)
+            {
+                return NotFound(); // Eğer kategori bulunamazsa, NotFound döndürülür.
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj) // Bu metod, Create sayfasında form gönderildiğinde tetiklenir.
+        {
+            //3 adet validation var bunlardan 1i client side javascript ile eklediğimiz sayfa yenilenmeden doğrulama yapar
+            //diğeri asp validation bu controllerdan model error ekleriz viewda <div asp-validation - summary = "All" ></ div > ekleriz
+            //custom validation modelden ekleriz 
+
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "İsim ile display order aynı olamaz."); // Eğer Name ve DisplayOrder aynı ise, ModelState'e özel bir hata eklenir.
+            }
+            if (obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("name", "Test ismi kullanılamaz."); // Eğer Name "test" ise, ModelState'e özel bir hata eklenir.
+            }
+            if (ModelState.IsValid)
+            {
+                //ModelState.IsValid kontrolü yapıldıktan sonra veritabanına ekleme işlemi yapılır.
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); //Eğer veritabanına ekleme işlemi başarılı ise Index sayfasına yönlendirme yapılır.
+            }
+            return View(); //Eğer ModelState.IsValid kontrolü başarısız ise, tekrar Create sayfasına yönlendirme yapılır.
+
+        }
+        public IActionResult Delete(int? id) // Bu metod, Edit sayfasına yönlendirme yapmak için kullanılır. id parametresi nullable int olarak tanımlanmıştır.
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound(); // Eğer id null veya 0 ise, NotFound döndürülür.
+            }
+            Category categoryFromDb = _db.Categories.Find(id); // Veritabanından id'ye göre kategori bulunur.
+            if (categoryFromDb == null)
+            {
+                return NotFound(); // Eğer kategori bulunamazsa, NotFound döndürülür.
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id) // Bu metod, Create sayfasında form gönderildiğinde tetiklenir.
+        {
+            Category obj = _db.Categories.Find(id); // Veritabanından id'ye göre kategori bulunur.
+            if(obj==null) // Eğer kategori bulunamazsa, NotFound döndürülür.
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj); // Kategori silinir.
+            _db.SaveChanges(); // Veritabanına kaydedilir.
+            return RedirectToAction("Index"); // Silme işlemi başarılı ise Index sayfasına yönlendirme yapılır
+        }
+
     }
 }
